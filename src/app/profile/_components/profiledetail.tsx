98c16f938d1b9d35adfac01f5f86c0b9b3883a4a
Shaxzoda <shakhzoda2404@gmail.com>
2025-01-22 02:44:51 +0500

    finished

--- src/app/profile/_components/profiledetail.tsx
+++ src/app/profile/_components/profiledetail.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import Logo from "../../../../public/icons/logo";
 export const Profiledetail = () => {
   return <div className="container">
   <div className="pt-[52px] flex items-center  ">
-    <Image
-      src={Logo}
-      alt="Profile Image"
-      width={163}
-      height={163}
-      className="rounded-full mr-[33px]"
-    />
+
     <div>
       <h1 className="font-[700] pb-[10px]  text-[30px] line-[133%]">
   Berdikulova Shaxzoda
